d986d6d0839bdcc4abc9477b285ed7350a826e1c
Kenpachi-Ikari <ronitdey7777@gmail.com>
2025-08-22 04:44:28 +0530

    Starting my web dev jourmey. Learning Javascript.

---
+++ test.js
@@ -0,0 +1,20 @@
+// var n = 2;
+// function square (num) {
+//     num = num * num;
+//     return num;
+// }
+
+// var square2 = square(12);
+// console.log(square2);
+// var square3 = square(n);
+// console.log(square3);
+
+getName();
+console.log(x);
+console.log(getName);
+
+var x = 5;
+// console.log(x); // This will log 'undefined' due to hoisting
+function getName() {
+    console.log("Hello, World!");
+}
