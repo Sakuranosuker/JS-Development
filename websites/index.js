be8143f4122aeaf3737709aa1cb1fb6692325c28
Kenpachi-Ikari <ronitdey7777@gmail.com>
2025-08-29 05:06:04 +0530

    * String operations ,  Loops , Functions etc.

--- websites/index.js
+++ websites/index.js
@@ -60,14 +60,14 @@
 // let circum = 2*pi*radius;
 // console.log(circum)
 
-document.getElementById("radH1").textContent = "Circumference";
-document.getElementById("radiusSubmit").onclick = function(){
-    const pi = 3.14159;
-    radius = document.getElementById("radiusInput").value;
-    radius = Number(radius);
-    circumference  = 2*pi*radius;
-    document.getElementById("radiusShow").textContent = `Circumference found is: ${circumference}`;
-}
+// document.getElementById("radH1").textContent = "Circumference";
+// document.getElementById("radiusSubmit").onclick = function(){
+//     const pi = 3.14159;
+//     radius = document.getElementById("radiusInput").value;
+//     radius = Number(radius);
+//     circumference  = 2*pi*radius;
+//     document.getElementById("radiusShow").textContent = `Circumference found is: ${circumference}`;
+// }
 
 //MATH OPERATIONS --
 
@@ -90,8 +90,110 @@ document.getElementById("radiusSubmit").onclick = function(){
 // console.log(random);
 
 // IF - Statements --
-let age = 25;
-if(age>18)
-    console.log(`You can enter the site`);
-else
-    console.log(`You cannot enter the site`); 
+// let age = 25;
+// if(age>18)
+//     console.log(`You can enter the site`);
+// else
+//     console.log(`You cannot enter the site`); 
+
+
+// Checkbox tutorial -- 
+let checkbox = document.getElementById("mycheckbox");
+let visa = document.getElementById("visa");
+let mastercard = document.getElementById("mastercard");
+let paypal = document.getElementById("paypal");
+
+let submit = document.getElementById("mysubmit");
+let operator = document.getElementById("operator-viewing");
+let payment = document.getElementById("payment-status");
+const check = document.getElementById("check");
+
+
+submit.onclick = function () {
+    if(checkbox.checked){
+        check.textContent = `You have checked the required button`;
+    }
+    else{
+        check.textContent = `You havent checked the button`;
+        payment.textContent = `Cannot proceed with payment`;
+        return;
+    }
+
+    if(visa.checked){
+        operator.textContent = `You have chosen VISA`;
+        payment.textContent = `Safe to proceed`;
+    }
+    else if (mastercard.checked){
+        operator.textContent = `You have chosen Mastercard`;
+        payment.textContent = `Safe to proceed`;
+    }
+    else if(paypal.checked){
+        operator.textContent = `You have chosen Paypal`
+        payment.textContent = `Safe to proceed`;
+    }
+    else{
+        operator.textContent = `You must select a payment type`;
+        payment.textContent = `Cannot procee with payment`;
+    }
+}
+
+// Ternary operator -
+// let age = 19;
+// console.log(age>18 ? (age>21 ? `Adult` : `Teen`) : `Minor`);
+
+
+//  String operations -
+// let username = "10Ronit";
+// if(username.startsWith(` `)){
+//     console.log(`Your username cannot start with a whitespace`);
+// }
+// else{
+// let i=0;
+// for (i = 0; i < 10; i++){
+//     if(username.startsWith(i.toString()))
+//     {
+//         console.log(`Your username cannot start with a number`);
+//         break;
+//     }
+// }
+// }
+
+// let fullname = "RONIT DEY"
+// let firstname = fullname.slice(0,5);
+// console.log(firstname);
+
+// let email = "ronitdey@gmail.com";
+// console.log(`The username is : ${email.slice(0, email.indexOf("@"))}`);
+// console.log(`The extension is: ${email.slice(email.indexOf("@")+1)}`); // all after the index of "@" in the string.
+
+
+// Method calling --
+// let username = "  roNItdeY  ";
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(username.charAt(0)+1).toLowerCase();
+// console.log(username);
+
+
+// Loops --
+// let loggedin = false;
+// let username;
+// let password;
+
+// while(!loggedin){
+//     username = window.prompt(`Enter your username:`);
+//     password = window.prompt(`Enter your paswsword:`);
+
+//     if(username == "Ronit" && password == "ronitdey1122"){
+//         loggedin = true;
+//         console.log("LOGGED IN SUCCESSFULLY");
+//     }
+//     else{
+//         console.log(`Wrong credentials try again`);
+//     }
+// }
+
+// Functions --
+function add(a,b){
+    let sum = a+b;
+    return sum;
+}
+console.log(add(20,69));
